e8c764187874ac2d3572b1c6e16aca61857b012d
sansidsac <kssanju04@gmail.com>
2024-10-06 01:18:47 +0530

    footer and exowiki

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
 import Home from './components/Home.jsx'
-import PlanetLayerApp from './pages/PlanetCreation/PlanetLayerApp.jsx';
+// import PlanetLayerApp from './pages/PlanetCreation/PlanetLayerApp.jsx';
 
 function App() {
   return (
     <>
-    <PlanetLayerApp/>
+    {/* <PlanetLayerApp/> */}
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="/create" element={<PlanetLayerApp/>}></Route>
+        {/* <Route path="/create" element={<PlanetLayerApp/>}></Route> */}
       </Routes>
     </>
   )
