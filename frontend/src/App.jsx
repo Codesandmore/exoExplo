3bcfb41eddcdf5a54fd3230c25012d0716eb3a01
Codesandmore <chackochanme@gmail.com>
2024-10-06 21:38:38 +0530

    story

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { Route,Routes } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import MemoryCardGame from './pages/Games/MemoryCardGame.jsx';
 import Quiz from './pages/Games/quiz.jsx';
-import PlanetTravel from './pages/ExoExplorer/ExoExplorer.jsx';
-import StoryBoard from './pages/StoryBoard/Story.jsx';
+import StoryBoard from './pages/StoryBoard/Story.jsx'; 
 import PlanetLayerApp from './pages/PlanetCreation/PlanetLayerApp.jsx';
+import PlanetTravel from './pages/ExoExplorer/ExoExplorer.jsx';
+import Lander from './pages/ExoWiki/Lander.jsx';
+import Lander2 from './pages/ExoWiki/Lander2.jsx'
 
 function App() {
   return (
@@ -14,8 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/create" element={<PlanetLayerApp/>}></Route>
+        <Route path="/travel" element={<PlanetTravel/>}></Route>
         <Route path="/memory" element={<MemoryCardGame/>}></Route>
         <Route path="/quiz" element={<Quiz/>}></Route>
+        <Route path="/story" element={<StoryBoard/>}></Route>
+        <Route path="/wiki" element={<Lander/>}></Route>
+        <Route path="/wiki2" element={<Lander2/>}></Route>
       </Routes>
     </>
   )
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className="flex flex-col w-full h-fit bg-[#091b38] text-[#e5e7eb] px-1 py-6">
+        <div className="flex flex-col w-full h-fit bg-[#091b38] text-[#e5e7eb] px-1 py-6 ">
     <div className="text-center">© 2024 UFO - All rights reserved.</div>
 </div>
     </div>
