23c24a860adcfc952aa1976e4c9de6814b2e1991
Codesandmore <chackochanme@gmail.com>
2024-10-06 22:10:36 +0530

    browserrouting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,29 @@
-import './App.css'
-import { Route,Routes } from 'react-router-dom'
-import Home from './components/Home.jsx'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home.jsx';
 import MemoryCardGame from './pages/Games/MemoryCardGame.jsx';
 import Quiz from './pages/Games/Quiz.jsx';
 import StoryBoard from './pages/StoryBoard/Story.jsx'; 
 import PlanetLayerApp from './pages/PlanetCreation/PlanetLayerApp.jsx';
 import PlanetTravel from './pages/ExoExplorer/ExoExplorer.jsx';
 import Lander from './pages/ExoWiki/Lander.jsx';
-import Lander2 from './pages/ExoWiki/Lander2.jsx'
+import Lander2 from './pages/ExoWiki/Lander2.jsx';
 
 function App() {
   return (
-    <>
-    {/* <PlanetLayerApp/> */}
+    <Router>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/create" element={<PlanetLayerApp/>}></Route>
-        <Route path="/travel" element={<PlanetTravel/>}></Route>
-        <Route path="/memory" element={<MemoryCardGame/>}></Route>
-        <Route path="/quiz" element={<Quiz/>}></Route>
-        <Route path="/story" element={<StoryBoard/>}></Route>
-        <Route path="/wiki" element={<Lander/>}></Route>
-        <Route path="/wiki2" element={<Lander2/>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<PlanetLayerApp />} />
+        <Route path="/travel" element={<PlanetTravel />} />
+        <Route path="/memory" element={<MemoryCardGame />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/story" element={<StoryBoard />} />
+        <Route path="/wiki" element={<Lander />} />
+        <Route path="/wiki2" element={<Lander2 />} />
       </Routes>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App;
